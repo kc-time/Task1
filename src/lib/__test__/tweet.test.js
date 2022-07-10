83d6151312f5860eb385a0dc88c2b0dc1e1256e5
kelvin-tatler <kelvin.chan@tatlerasia.com>
2022-07-09 23:35:31 -0400

    Completed Tweet test cases

--- src/lib/__test__/tweet.test.js
+++ src/lib/__test__/tweet.test.js
@@ -1,6 +1,13 @@
 const request = require("supertest");
 const app = require("../../app");
 import mongoose from "mongoose";
+var session = require('supertest-session');
+ 
+var testSession = null;
+ 
+beforeEach(function () {
+  testSession = session(app);
+});
 
 let random = Math.random().toString().substr(2, 8);
 
@@ -38,6 +45,82 @@ describe("Test the tweet path", () => {
       .expect(401);
   });
 
+  test("Test user register", () => {
+    return request(app)
+      .post("/users")
+      .send({
+          username: 'kelvinchan'+ random,
+          password: 'qwe123asd',
+        })
+      .expect(201);
+  });
+
+});
+
+describe('after login', function () {
+ 
+  var authenticatedSession;
+  let tweetId;
+ 
+  beforeEach(function (done) {
+    testSession.post('/users/login')
+      .send({ username: 'kelvinchan' + random,
+        password: 'qwe123asd'
+      })
+      .expect(200)
+      .end(function (err) {
+        if (err) return done(err);
+        authenticatedSession = testSession;
+        return done();
+      });
+  });
+ 
+  it('create tweet', function (done) {
+    authenticatedSession.post("/tweets")
+      .send({
+          content: "this is content"
+        })
+      .expect((res) => {
+        tweetId = res._body._id
+      })
+      .expect(201)
+      .end(done)
+  });
+
+  it('read tweet', function (done) {
+    authenticatedSession.get("/tweets/" + tweetId)
+      .expect(200)
+      .end(done)
+  });
+
+  it('update fake tweet', function (done) {
+    authenticatedSession.put("/tweets/sljahfiudabfsaef")
+      .send({
+          content: "this is content2"
+        })
+      .expect(400)
+
+      .end(done)
+  });
+
+  it('update tweet', function (done) {
+    authenticatedSession.put("/tweets/" + tweetId)
+      .send({
+          content: "this is content2"
+        })
+      .expect((res) => {
+        return res.body.content === "this is content2"
+      })
+      .expect(200)
+
+      .end(done)
+  });
+
+  it('delete tweet', function (done) {
+    authenticatedSession.delete("/tweets/" + tweetId)
+      .expect(200)
+      .end(done)
+  });
 
 });
 
